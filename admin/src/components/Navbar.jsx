23f4424276e343e078271c9db24e45fbf7706bbb
Naveen <naveenu4321@gmail.com>
2026-09-02 20:27:02 +0530

    admin to home page redirect

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -12,13 +12,16 @@ const Navbar = () => {
 
   const navigate = useNavigate()
 
-  const logout = () =>{
-    navigate('/')
-    aToken && setAToken('')
-    aToken && localStorage.removeItem('aToken')
-    dToken && setDToken('')
-    dToken && localStorage.removeItem('dToken')
-  }
+  const logout = () => {
+
+  aToken && setAToken('')
+  aToken && localStorage.removeItem('aToken')
+
+  dToken && setDToken('')
+  dToken && localStorage.removeItem('dToken')
+
+  window.location.href = 'https://mind-well-client.vercel.app'
+}
 
   return (
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
